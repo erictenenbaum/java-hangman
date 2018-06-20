b9b0d243709eca537c0bc8c22e2e2bc15a860af8
erictenenbaum <erictenenbaum10@gmail.com>
2018-06-19 20:27:31 -0700

    finished the game

--- HangMan/src/Game.java
+++ HangMan/src/Game.java
@@ -1,29 +1,13 @@
+import org.ietf.jgss.GSSManager;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class Game {
 
     public static  void main (String[] args){
         HangMan varOne = new HangMan();
-
-       System.out.println(varOne.getWord());
-
-       System.out.println(Arrays.toString(varOne.splitWord()));
-
-       System.out.println(Arrays.toString(varOne.dashArr()));
-
-       Scanner input = new Scanner(System.in);
-
-        System.out.println("Please select a letter");
-        String guessedLetter = input.nextLine();
-
-        System.out.println("You guessed " + guessedLetter);
-
-        System.out.println(varOne.checkGuess(guessedLetter));
-
-
-
-
-
-
+//       System.out.println(varOne.getWord());
+       varOne.startGame();
     }
 }
--- HangMan/src/HangMan.java
+++ HangMan/src/HangMan.java
@@ -6,10 +6,8 @@ import java.util.ArrayList;
 public class HangMan {
     private String hiddenWord;
     private String[] gameArray;
-
-//    public HangMan(String word){
-//        this.hiddenWord = word;
-//    }
+    private ArrayList<String> previouslyGuessedLetters;
+    private int remainingGuesses;
 
     public HangMan(){
         String wordBank[] = {"wolf", "cheetah", "pig", "goat"};
@@ -20,10 +18,30 @@ public class HangMan {
         this.hiddenWord = wordBank[n];
 
         this.gameArray = Arrays.copyOf(this.dashArr(), this.dashArr().length);
+        this.remainingGuesses = 9;
+        this.previouslyGuessedLetters = new ArrayList<String>();
     }
 
    public String getWord(){
        return this.hiddenWord;
+   }
+
+   public ArrayList<String> getPreviouslyGuessedLetters(){
+        return this.previouslyGuessedLetters;
+   }
+
+   public int getRemainingGuesses(){
+        return  this.remainingGuesses;
+   }
+
+   public String[] getGameArray() {return  this.gameArray;}
+
+   public void addPreviouslyGuessedLetter(String letter){
+        this.previouslyGuessedLetters.add(letter);
+   }
+
+   public void takeAGuessAway () {
+        this.remainingGuesses--;
    }
 
    public void setWord(String word) {
@@ -41,27 +59,6 @@ public class HangMan {
 
         return dashes;
    }
-
-//   public String[] checkGuess (String letter){
-//        System.out.println("Letter to check " + letter);
-//       String[] tempDashes = Arrays.copyOf(this.dashArr(), this.dashArr().length);
-//       String[] referenceArray = this.splitWord();
-//
-//
-//
-//       for(int i = 0; i < referenceArray.length; i++){
-//           if(referenceArray[i].equals(letter)){
-//               System.out.println("it worked!");
-//               tempDashes[i] = letter;
-//           }
-//
-//
-//       }
-//
-//
-//       return tempDashes;
-//
-//   }
 
 //    need to switch this to an ArrayList and push the matched indexes
    public ArrayList<Integer> checkGuess(String letter){
@@ -81,9 +78,82 @@ public class HangMan {
    }
 
 
-   public void replaceLetter(int index, String letter){
-        this.gameArray[index] = letter;
+   public void replaceLetter(ArrayList<Integer> indecies, String letter){
+
+        for(int i = 0; i < indecies.size(); i++){
+//            this.gameArray[index] = letter;
+
+//            System.out.println(indecies.get(i));
+
+            this.gameArray[indecies.get(i)] = letter;
+        }
+
    }
+
+   public boolean handleGuesss(String letter){
+        if(this.checkGuess(letter).size() == 0){
+            this.takeAGuessAway();
+            System.out.println("Sorry that letter is not in the word!");
+            this.addPreviouslyGuessedLetter(letter);
+
+//            checking if win has occurred otherwise return false
+
+            if(this.getRemainingGuesses() > 0){
+                return false;
+            }else{
+                return true;
+            }
+
+        }
+        else {
+            this.replaceLetter(this.checkGuess(letter), letter);
+
+            if(Arrays.equals(this.gameArray, this.splitWord())){
+                return true;
+            }else {
+                return false;
+            }
+
+        }
+   }
+
+
+    public  String askForLetter(String[] gameArray, ArrayList<String> previouslyGuessedLetters, int remainingGuesses){
+        Scanner input = new Scanner(System.in);
+        System.out.println(Arrays.toString(gameArray));
+        System.out.println("Previously guessed letters: " +  previouslyGuessedLetters);
+        System.out.println("Remaining Guesses: " + remainingGuesses);
+
+        System.out.println("Please guess a letter");
+
+        String guessedLetter = input.nextLine();
+
+        System.out.println("You guessed " + guessedLetter);
+
+        return guessedLetter;
+    }
+
+
+    public void continueFunction (boolean conditional){
+        if(conditional){
+
+            if(this.getRemainingGuesses() > 0){
+                System.out.println("Congats you won!");
+            }
+            else{
+                System.out.println("You lost!");
+            }
+
+        }
+        else{
+          continueFunction(this.handleGuesss(this.askForLetter(this.getGameArray(), this.getPreviouslyGuessedLetters(), this.getRemainingGuesses())));
+        }
+    }
+
+    public void startGame(){
+        continueFunction(this.handleGuesss(this.askForLetter(this.getGameArray(), this.getPreviouslyGuessedLetters(), this.getRemainingGuesses())));
+    }
+
 
 }
 
